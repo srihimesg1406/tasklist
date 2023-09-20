1dcc98f7cd15d5ac7b5c13d63e7e75ca6eaeb53c
srihi <srihimesh@gmail.com>
2023-09-20 16:04:25 -0500

    Added rows to DB

--- db.py
+++ db.py
@@ -7,8 +7,15 @@ db_host= "localhost"
 def getTaskList():
     conn = psycopg2. connect(dbname=db_name, user=db_user, password=db_pw, host=db_host)
     cur = conn.cursor()
-    cur.execute('SELECT task_name, is_active FROM public."TaskList"')
+    cur.execute('SELECT task_name, is_done FROM public."TaskList"')
     tasklist = cur.fetchall()
     cur.close
     conn.close
     return tasklist
+
+def addTask(name, date):
+    conn = psycopg2.connect(dbname=db_name, user=db_user, password=db_pw, host=db_host)
+    cur = conn.cursor()
+    cur.execute('INSERT INTO public."TaskList"(task_name,due_date) values(\'%s\',\'%s\');commit;' % (name,date))
+    cur.close
+    conn.close
--- main.py
+++ main.py
@@ -1,11 +1,19 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, request, redirect, url_for
+from db import getTaskList, addTask
 app = Flask(__name__)
 
-tasklist = [["Walk Dog", True],["Wash Dishes", False],["Take out trash", True]]
+#tasklist = [["Walk Dog", True],["Wash Dishes", False],["Take out trash", True]]
+tasklist=getTaskList()
 @app.route("/")
 def home():
+    tasklist = getTaskList()
     return render_template("tasklist.html", TaskList=tasklist)
 
+@app.route("/add", methods=['POST'])
+def add():
+    taskname = request.form['TaskName']
+    duedate = request.form['DueDate']
+    addTask(taskname,duedate)
+    return redirect(url_for('home'))
 
 app.run()
